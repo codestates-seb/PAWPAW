8c6b4f2338cf2f36fe0ff516ee7b3030e024dd5a
LEEKH109 <dlrjsgml109@gmail.com>
2023-01-09 14:38:23 +0900

    feat: modal img index 추가

---
+++ client/src/img/modal.tsx
@@ -0,0 +1,7 @@
+import MarkerIcon from './markerIcon.svg';
+import TimeIcon from './timeIcon.svg';
+import PhoneIcon from './phoneIcon.svg';
+import HomeIcon from './homeIcon.svg';
+import ModalBookmarkIcon from './modalBookmarkIcon.svg';
+
+export { MarkerIcon, TimeIcon, PhoneIcon, HomeIcon, ModalBookmarkIcon };
