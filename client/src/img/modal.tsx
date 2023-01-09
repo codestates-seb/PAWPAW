c9a882ded5f949d03b48ef5c6ad6b66967b37a24
LEEKH109 <dlrjsgml109@gmail.com>
2023-01-10 00:49:05 +0900

    fix: svg 수정

--- client/src/img/modal.tsx
+++ client/src/img/modal.tsx
@@ -2,6 +2,7 @@ import MarkerIcon from './markerIcon.svg';
 import TimeIcon from './timeIcon.svg';
 import PhoneIcon from './phoneIcon.svg';
 import HomeIcon from './homeIcon.svg';
-import ModalBookmarkIcon from './modalBookmarkIcon.svg';
+import ModalBookmarkIcon from './modalbookmarkIcon.svg';
+import ModalSample from './modalSample.svg';
 
-export { MarkerIcon, TimeIcon, PhoneIcon, HomeIcon, ModalBookmarkIcon };
+export { MarkerIcon, TimeIcon, PhoneIcon, HomeIcon, ModalBookmarkIcon, ModalSample };
