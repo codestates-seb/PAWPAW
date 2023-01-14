500fe260db08c648e07cd2608d1003cb2700c4ca
LEEKH109 <dlrjsgml109@gmail.com>
2023-01-15 03:44:50 +0900

    [FE] feat: petUpdate, petDelete 기능 완성

--- client/src/util/UserApi.tsx
+++ client/src/util/UserApi.tsx
@@ -1,34 +1,60 @@
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 const jwtToken = localStorage.getItem('Authorization');
 const refreshToken = localStorage.getItem('Refresh');
-axios.defaults.headers.common['Authorization'] = `${jwtToken}`;
-axios.defaults.headers.common['Refresh'] = `${refreshToken}`;
-const url = 'http://localhost:8080';
+// axios.defaults.headers.common['Authorization'] = `${jwtToken}`;
+// axios.defaults.headers.common['Refresh'] = `${refreshToken}`;
+const url = '';
 
-export const userUpdate = async (
-  id: string,
+export const petUpdate = async (
+  petId: string,
   petname: string,
-  age: string,
+  age: number,
   gender: string,
+  species: string,
   address: string,
+  formData: { profileImage: string | Blob },
 ) => {
+  const navigate = useNavigate();
+  if (!formData.profileImage) return;
+  const headers = {
+    'Content-Type': 'multipart/form-data',
+    Authorization: jwtToken,
+    Refresh: refreshToken,
+  };
+  const data = new FormData();
+  data.append('petName', petname);
+  data.append('age', age.toString());
+  data.append('species', species);
+  data.append('gender', gender);
+  data.append('address', address);
+  data.append('profileImage', formData.profileImage);
+  console.log(data);
+  console.log(formData);
+  console.log(formData.profileImage);
+  for (const key of data.keys()) {
+    console.log(key);
+  }
+  for (const value of data.values()) {
+    console.log(value);
+  }
   try {
-    await axios.patch(`${url}/pets/${id}`, {
-      petname: petname,
-      age: age,
-      gender: gender,
-      address: address,
-      profileImage: '',
-    });
-    window.location.reload();
+    await axios.post(`${url}/patch/${petId}`, data, { headers });
+    navigate('/login');
+    // 비동기 에러 날 것 같으면 .then 사용
   } catch (error) {
     console.error('Error', error);
+    alert(error);
   }
 };
 
-export const userDelete = async (id: string) => {
+export const petDelete = async (id: string) => {
+  const headers = {
+    Authorization: jwtToken,
+    Refresh: refreshToken,
+  };
   try {
-    await axios.delete(`${url}/pets/${id}`);
+    await axios.delete(`${url}/pets/${id}`, { headers });
   } catch (error) {
     console.error('Error', error);
   }
