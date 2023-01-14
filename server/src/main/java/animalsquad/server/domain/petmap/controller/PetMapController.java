5e48c1fa1ff76befc66d998d50e125cc3dbea159
khd325 <hdk325@naver.com>
2023-01-14 23:24:40 +0900

    [BE] feat: token에서 @AuthenticationPrincipal으로 변경

--- server/src/main/java/animalsquad/server/domain/petmap/controller/PetMapController.java
+++ server/src/main/java/animalsquad/server/domain/petmap/controller/PetMapController.java
@@ -4,9 +4,11 @@ import animalsquad.server.domain.petmap.dto.MyPlaceDto;
 import animalsquad.server.domain.petmap.entity.PetMap;
 import animalsquad.server.domain.petmap.mapper.PetMapMapper;
 import animalsquad.server.domain.petmap.service.PetMapService;
+import animalsquad.server.global.auth.userdetails.PetDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,20 +21,22 @@ public class PetMapController {
 
     @PostMapping("/maps/addplace")
     public ResponseEntity createPlace(@RequestBody MyPlaceDto addPlaceDto,
-                                      @RequestHeader("Authorization") String token) {
+                                      @AuthenticationPrincipal PetDetailsService.PetDetails principal) {
 
+        long petId = principal.getId();
         PetMap petMap = mapper.myPlaceDtoToPetMap(addPlaceDto);
-        PetMap result = petMapService.addPlace(petMap, token);
+        PetMap result = petMapService.addPlace(petMap, petId);
 
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
     @DeleteMapping("/maps/cancel")
     public ResponseEntity deletePlace(@RequestBody MyPlaceDto deletePlaceDto,
-                                      @RequestHeader("Authorization") String token) {
+                                      @AuthenticationPrincipal PetDetailsService.PetDetails principal) {
 
         PetMap petMap = mapper.myPlaceDtoToPetMap(deletePlaceDto);
-        petMapService.deletePlace(petMap,token);
+        long petId = principal.getId();
+        petMapService.deletePlace(petMap,petId);
 
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
