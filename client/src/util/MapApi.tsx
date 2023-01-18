27f139ca35d8308fe77ff74de54c9c91bb3ca7f6
LEEKH109 <dlrjsgml109@gmail.com>
2023-01-18 10:51:45 +0900

    [FE] feat: Mapapi 삭제기능 추가

--- client/src/util/MapApi.tsx
+++ client/src/util/MapApi.tsx
@@ -2,3 +2,17 @@ import axios from 'axios';
 const jwtToken = localStorage.getItem('Authorization');
 const refreshToken = localStorage.getItem('Refresh');
 const url = '';
+const headers = {
+  Authorization: jwtToken,
+  Refresh: refreshToken,
+};
+
+export const mapReviewDelete = async (reviewid: any) => {
+  try {
+    await axios.delete(`${url}/maps/review/${reviewid}`, { headers });
+  } catch (error) {
+    console.error('Error', error);
+  } finally {
+    window.location.reload();
+  }
+};
