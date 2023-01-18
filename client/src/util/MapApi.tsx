0b75f9fea07ee7d5b72e34ffbd000da625dce00f
LEEKH109 <dlrjsgml109@gmail.com>
2023-01-18 13:00:23 +0900

    [FE] feat: Mapapi 업데이트 기능 추가

--- client/src/util/MapApi.tsx
+++ client/src/util/MapApi.tsx
@@ -1,18 +1,34 @@
 import axios from 'axios';
 const jwtToken = localStorage.getItem('Authorization');
 const refreshToken = localStorage.getItem('Refresh');
+const petId = localStorage.getItem('petId') as string;
 const url = '';
 const headers = {
   Authorization: jwtToken,
   Refresh: refreshToken,
 };
 
-export const mapReviewDelete = async (reviewid: any) => {
+export const mapReviewDELETE = async (reviewid: any) => {
   try {
     await axios.delete(`${url}/maps/review/${reviewid}`, { headers });
+    window.location.reload();
   } catch (error) {
     console.error('Error', error);
-  } finally {
+  }
+};
+
+export const mapReviewUPDATE = async (reviewid: any, contents: string) => {
+  try {
+    await axios.patch(
+      `${url}/maps/review/${reviewid}`,
+      {
+        petId: petId,
+        contents: contents,
+      },
+      { headers },
+    );
     window.location.reload();
+  } catch (error) {
+    console.error('Error', error);
   }
 };
