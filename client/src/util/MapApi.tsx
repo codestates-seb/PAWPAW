1f363ca3634d587383f2feda5d0f7f7b5c570f2a
LEEKH109 <dlrjsgml109@gmail.com>
2023-01-18 13:04:38 +0900

    [FE] feat: Mapapi edit 기능 추가

--- client/src/util/MapApi.tsx
+++ client/src/util/MapApi.tsx
@@ -8,16 +8,23 @@ const headers = {
   Refresh: refreshToken,
 };
 
-export const mapReviewDELETE = async (reviewid: any) => {
+export const mapReviesEdit = async (reviewid: number, infoMapId: number, contents: string) => {
   try {
-    await axios.delete(`${url}/maps/review/${reviewid}`, { headers });
-    window.location.reload();
+    await axios.post(
+      `${url}/maps/review/${reviewid}`,
+      {
+        petId: petId,
+        infoMapId: infoMapId,
+        contents: contents,
+      },
+      { headers },
+    );
   } catch (error) {
     console.error('Error', error);
   }
 };
 
-export const mapReviewUPDATE = async (reviewid: any, contents: string) => {
+export const mapReviewUPDATE = async (reviewid: number, contents: string) => {
   try {
     await axios.patch(
       `${url}/maps/review/${reviewid}`,
@@ -27,6 +34,15 @@ export const mapReviewUPDATE = async (reviewid: any, contents: string) => {
       },
       { headers },
     );
+    window.location.reload();
+  } catch (error) {
+    console.error('Error', error);
+  }
+};
+
+export const mapReviewDELETE = async (reviewid: number) => {
+  try {
+    await axios.delete(`${url}/maps/review/${reviewid}`, { headers });
     window.location.reload();
   } catch (error) {
     console.error('Error', error);
