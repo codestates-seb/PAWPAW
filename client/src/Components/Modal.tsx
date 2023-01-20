22e7f84d000571e4d7c728b26797b770a32e6596
SuJin <1127sujink@naver.com>
2023-01-20 13:18:35 +0900

    [FE] chore: 필요없는 코드 삭제

--- client/src/Components/Modal.tsx
+++ client/src/Components/Modal.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { Icon } from '@iconify/react';
 import axios from 'axios';
-
 import ModalSample from '../img/modalSample.svg';
 import UserImg1 from '../img/UserImg1.png';
 import color from '../color';
@@ -55,7 +54,6 @@ const Modal = ({ click, setClick, id, bookmark }: CProps['clicks']) => {
   const [review, setReview] = useState<string>('');
   const [editReview, setEditReview] = useState<string>('');
   const [editActivate, setEditActivate] = useState<number>(0);
-  const [count, setCount] = useState<number>(0);
   const [myPick, setMyPick] = useState<boolean>(bookmark);
   const [mapdata, setMapdata] = useState<MapData>({
     details: {
@@ -121,15 +119,6 @@ const Modal = ({ click, setClick, id, bookmark }: CProps['clicks']) => {
   const selectHandler = () => {
     setClick(!click);
   };
-
-  if (count === 0 && resData !== null) {
-    const { details, reviews, pageInfo } = resData as MapData;
-    console.log('resData', resData);
-    setMapdata({ details: details, reviews: reviews, pageInfo: pageInfo });
-    setCount(count + 1);
-    console.log('mapdata', mapdata); // detail, pageInfo는 있는데 reviews가 안담김
-    console.log('mapdata.reviews', mapdata.reviews);
-  }
 
   const reviewHandler = (e: React.ChangeEvent<HTMLInputElement>): void => {
     setReview((e.target as HTMLInputElement).value);
