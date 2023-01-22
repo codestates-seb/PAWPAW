9f3c3ac72ac577064d280c03788f044287f18fe3
LEEKH109 <dlrjsgml109@gmail.com>
2023-01-22 19:50:25 +0900

    [FE] feat: 헤더 커뮤니티 라우팅 설정, 커뮤니터 초기 설정

--- client/src/Pages/Community.tsx
+++ client/src/Pages/Community.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import Header from '../Components/Header';
+import styled from 'styled-components';
 
-const Community = () => {return (
-    <div></div>
+const Community:React.FC = () => {return (
+    <Container>
+        <Header/>
+    </Container>
   );
 };
 
 export default Community;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  /* display: flex;
+  justify-content: center;
+  align-items: center; */
+`;
