bf57ad6cee27ba3cccc6a081a7c1eb399267e306
JaneChun <janechun22@gmail.com>
2023-01-25 17:42:12 +0900

    [FE] feat: 글 작성 페이지 구현

--- client/src/Pages/Post.tsx
+++ client/src/Pages/Post.tsx
@@ -1,48 +1,77 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 import styled from 'styled-components';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { Icon } from '@iconify/react';
 
 import Header from '../Components/Header';
 import Nav from '../Components/Nav';
 import color from '../color';
-import Editor from '../Components/Editor';
-import headers from '../util/headers';
 const { brown, darkbrown, bordergrey, lightgrey, red } = color;
 const petId = localStorage.getItem('petId');
+const jwtToken = localStorage.getItem('Authorization');
+const refreshToken = localStorage.getItem('Refresh');
+const headers = {
+  'Content-Type': 'multipart/form-data',
+  Authorization: jwtToken,
+  Refresh: refreshToken,
+};
 
 export interface IPost {
   petId: string | null;
   title: string;
   contents: string;
-  tag: string;
 }
 
 const Post: React.FC = () => {
   const navigate = useNavigate();
-  const [title, setTitle] = useState<string>('');
-  const [contents, setContents] = useState<string>('');
   const [data, setData] = useState<IPost>({
     petId: petId,
     title: '',
     contents: '',
-    tag: '자유',
   });
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
+  const [file, setFile] = useState<any>(null);
+  const formData = new FormData();
 
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setTitle(e.target.value);
+  const titleHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setData({ ...data, title: e.target.value });
+  };
+
+  const contentsHandler = (e: string) => {
+    setData({ ...data, contents: e });
+  };
+
+  const imageHandler = () => {
+    const input = document.createElement('input'); // input 태그를 동적으로 생성
+
+    input.setAttribute('type', 'file');
+    input.setAttribute('accept', 'image/*');
+    input.click();
+
+    input.onchange = () => {
+      if (input.files) {
+        // 파일로 만들어 file 상태에 저장
+        const file = input.files[0];
+        setFile(file);
+
+        // 에디터 아래에 이미지 띄우기
+        const imageURL = URL.createObjectURL(file);
+        setImageUrl(imageURL);
+      }
+    };
   };
 
   const submitHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    setData({ ...data, title: title, contents: contents });
 
     if (data.petId && data.title && data.contents) {
-      const formData = new FormData();
       formData.append('petId', data.petId);
       formData.append('title', data.title);
       formData.append('contents', data.contents);
-      formData.append('tag', data.tag);
+      formData.append('file', file);
 
       for (const key of formData.keys()) {
         console.log(key);
@@ -55,15 +84,77 @@ const Post: React.FC = () => {
         .post(`${process.env.REACT_APP_API_ROOT}/posts`, formData, { headers })
         .then((res) => {
           console.log(res);
-          // navigate('/');
+          // navigate('/community');
         })
         .catch((err) => alert(err));
+    } else {
+      alert('제목과 본문을 입력해주세요.');
     }
   };
 
   const cancelHandler = () => {
     navigate('/');
   };
+
+  const modules = useMemo(() => {
+    return {
+      toolbar: {
+        container: [
+          [{ size: ['false', 'large', 'huge'] }],
+          [{ align: [] }],
+          ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+          [{ list: 'ordered' }, { list: 'bullet' }],
+          [
+            {
+              color: [
+                '#000000',
+                '#e60000',
+                '#ff9900',
+                '#ffff00',
+                '#008a00',
+                '#0066cc',
+                '#9933ff',
+                '#ffffff',
+                '#facccc',
+                '#ffebcc',
+                '#ffffcc',
+                '#cce8cc',
+                '#cce0f5',
+                '#ebd6ff',
+                '#bbbbbb',
+                '#f06666',
+                '#ffc266',
+                '#ffff66',
+                '#66b966',
+                '#66a3e0',
+                '#c285ff',
+                '#888888',
+                '#a10000',
+                '#b26b00',
+                '#b2b200',
+                '#006100',
+                '#0047b2',
+                '#6b24b2',
+                '#444444',
+                '#5c0000',
+                '#663d00',
+                '#666600',
+                '#003700',
+                '#002966',
+                '#3d1466',
+                'custom-color',
+              ],
+            },
+            { background: [] },
+          ],
+          ['image', 'link'],
+        ],
+        handlers: {
+          image: imageHandler,
+        },
+      },
+    };
+  }, []);
 
   return (
     <Container>
@@ -74,20 +165,42 @@ const Post: React.FC = () => {
           <div>
             <Title>제목</Title>
             <TitleInput
-              value={title}
-              onChange={onChange}
+              value={data.title}
+              onChange={titleHandler}
               type='text'
-              placeholder='제목을 작성해주세요'
+              placeholder='제목을 작성해주세요.'
             />
           </div>
           <div>
             <Title className='body'>본문</Title>
           </div>
-          <Editor data={data} setContents={setContents} />
-          <ButtonsDiv>
-            <SubmitButton onClick={submitHandler}>등록</SubmitButton>
-            <CancelButton onClick={cancelHandler}>취소</CancelButton>
-          </ButtonsDiv>
+          <EditorContainer>
+            <ReactQuill
+              onChange={contentsHandler}
+              modules={modules}
+              placeholder='사진은 최대 1장만 첨부할 수 있어요.'
+            />
+          </EditorContainer>
+          <FooterDiv>
+            <ImageDiv>
+              {imageUrl ? (
+                <Image src={imageUrl.toString()} />
+              ) : (
+                <EmptyDiv>
+                  <Icon
+                    onClick={imageHandler}
+                    icon='ic:baseline-plus'
+                    color='white'
+                    style={{ fontSize: '60px' }}
+                  />
+                </EmptyDiv>
+              )}
+            </ImageDiv>
+            <ButtonsDiv>
+              <SubmitButton onClick={submitHandler}>등록</SubmitButton>
+              <CancelButton onClick={cancelHandler}>취소</CancelButton>
+            </ButtonsDiv>
+          </FooterDiv>
         </PostContainer>
       </Body>
     </Container>
@@ -137,6 +250,61 @@ const TitleInput = styled.input`
   &::placeholder {
     color: ${lightgrey};
   }
+`;
+
+const EditorContainer = styled.div`
+  .ql-editor {
+    height: 500px;
+    font-size: 18px;
+  }
+
+  // placeholder
+  .quill > .ql-container > .ql-editor.ql-blank::before {
+    font-size: 18px;
+    font-style: normal;
+    color: ${lightgrey};
+  }
+
+  // border
+  .ql-toolbar {
+    border-top-left-radius: 5px;
+    border-top-right-radius: 5px;
+    border-color: ${bordergrey};
+  }
+  .ql-container {
+    border-bottom-left-radius: 5px;
+    border-bottom-right-radius: 5px;
+    border-color: ${bordergrey};
+  }
+`;
+
+const FooterDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+`;
+
+const ImageDiv = styled.div``;
+
+const Image = styled.img`
+  margin-top: 15px;
+  max-width: 300px;
+  max-height: 300px;
+  border-radius: 20px;
+  object-fit: cover;
+`;
+
+const EmptyDiv = styled.div`
+  margin-top: 15px;
+  width: 100px;
+  height: 100px;
+  border-radius: 20px;
+  background-color: ${bordergrey};
+  cursor: pointer;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const ButtonsDiv = styled.div`
