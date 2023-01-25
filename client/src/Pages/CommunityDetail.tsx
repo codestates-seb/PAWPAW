ea5089a928f925de5cdee03664acddc526b29d05
LEEKH109 <dlrjsgml109@gmail.com>
2023-01-26 03:15:58 +0900

    [FE] feat: 게시글 상세 페이지 통신 양식 초안

--- client/src/Pages/CommunityDetail.tsx
+++ client/src/Pages/CommunityDetail.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import color from '../color';
@@ -6,9 +6,29 @@ import axios from 'axios';
 import headers from '../util/headers';
 import Header from '../Components/Header';
 
+const url = process.env.REACT_APP_API_ROOT;
+const petId = localStorage.getItem('petId') as string;
+
 const CommunityDetail: React.FC = () => {
+  const [postDetail, setPostDetail] = useState<object>({});
   const postId = useParams();
   console.log(postId);
+  useEffect(() => {
+    getData();
+    console.log('resetCheck');
+  }, []);
+
+  async function getData() {
+    await axios
+      .get(`${url}/posts/${postId}`, { headers })
+      .then((res) => {
+        setPostDetail(res.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
   return (
     <>
       <Header />
