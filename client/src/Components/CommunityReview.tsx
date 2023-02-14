2db360f4b4cc97196c9e180e3072b1b2d580b401
JaneChun <janechun22@gmail.com>
2023-02-15 00:51:24 +0900

    refactor: CommunityDetail - 컴포넌트 분리, 변수명 수정

---
+++ client/src/Components/CommunityReview.tsx
@@ -0,0 +1,227 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Swal from 'sweetalert2';
+import color from '../util/color';
+import { PostReviewEdit } from '../util/PostReviewApi';
+import { PostList, UserList } from '../Pages/CommunityDetail';
+import Review from './Review';
+
+type CProps = {
+  getData(): void;
+  postId: string | undefined;
+  postDetail: PostList;
+  userData: UserList;
+};
+
+const { brown, bordergrey, lightgrey, darkbrown } = color;
+
+const CommunityReview = ({ getData, postId, postDetail, userData }: CProps) => {
+  const [review, setReview] = useState<string>('');
+  const [editingCommentId, setEditingCommentId] = useState<number>(0);
+
+  const reviewHandler = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setReview((e.target as HTMLInputElement).value);
+  };
+
+  const reviewPostHandler = () => {
+    if (review === '') {
+      Swal.fire({
+        position: 'center',
+        icon: 'warning',
+        iconHtml: '⚠',
+        title: '내용을 입력해주세요. ',
+        color: brown,
+        padding: '20px 0px 40px 0px',
+      });
+      return;
+    } else {
+      PostReviewEdit(Number(postId), review).then(() => getData());
+      Swal.fire({
+        position: 'center',
+        icon: 'warning',
+        iconHtml: '🐾',
+        title: '작성되었습니다.',
+        color: brown,
+        padding: '20px 0px 40px 0px',
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      setReview('');
+    }
+  };
+
+  return (
+    <Container>
+      <Title>댓글 {postDetail.comments?.length}</Title>
+      {postDetail.comments && (
+        <Reviews>
+          {postDetail.comments.length === 0 ? (
+            <EmptyMessage>
+              댓글이 없어요.. <br />첫 번째 댓글을 남겨주세요 🐾
+            </EmptyMessage>
+          ) : (
+            postDetail.comments.map((comment: any) => (
+              <Review
+                key={comment.commentId}
+                comment={comment}
+                getData={getData}
+                editingCommentId={editingCommentId}
+                setEditingCommentId={setEditingCommentId}
+              />
+            ))
+          )}
+        </Reviews>
+      )}
+
+      <ReviewWrite>
+        <ReviewUserBox>
+          <ReviewUserImage src={userData.petInfo.profileImage} />
+          <ReviewUserName>{userData.petInfo.petName}</ReviewUserName>
+        </ReviewUserBox>
+        <ReviewInputTextBox>
+          <ReviewInputBox>
+            <ReviewInput
+              type='text'
+              value={review}
+              placeholder='댓글을 남겨주세요. '
+              onChange={reviewHandler}
+            />
+          </ReviewInputBox>
+          <ReviewButton onClick={reviewPostHandler}>작성</ReviewButton>
+        </ReviewInputTextBox>
+      </ReviewWrite>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  margin-top: 10px;
+  background-color: white;
+`;
+
+const Reviews = styled.div`
+  height: calc(100vh - 537px - 50px - 100px);
+  overflow-y: scroll;
+  ::-webkit-scrollbar {
+    width: 8px;
+  }
+  ::-webkit-scrollbar-track {
+    background-color: #dccdc8;
+    border-radius: 100px;
+  }
+  ::-webkit-scrollbar-thumb {
+    background-color: #a9908d;
+    border-radius: 100px;
+  }
+`;
+
+const Title = styled.div`
+  color: ${brown};
+  font-size: 20px;
+  font-weight: 800;
+  padding: 15px 10px;
+`;
+
+const ReviewUserBox = styled.div`
+  width: 70px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ReviewUserImage = styled.img`
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  background-size: cover;
+`;
+
+const ReviewUserName = styled.div`
+  margin-top: 8px;
+  color: ${brown};
+  font-size: 14px;
+  font-weight: Bold;
+`;
+
+const ReviewInputBox = styled.div`
+  flex-grow: 1;
+  color: ${brown};
+  font-size: 14px;
+  font-weight: 500;
+`;
+
+const ReviewInput = styled.input<Props>`
+  padding: 10px;
+  width: 100%;
+  height: 50px;
+  font-size: 14px;
+  color: ${brown};
+  border: 1px solid ${bordergrey};
+  border-radius: 15px;
+  resize: none;
+
+  &:focus {
+    outline: 1px solid ${bordergrey};
+  }
+  &::placeholder {
+    color: ${lightgrey};
+  }
+`;
+const ReviewButton = styled.button`
+  margin-left: 4px;
+  margin-right: 4px;
+  padding: 7px 10px;
+  font-weight: bold;
+  background: ${brown};
+  border-radius: 12px;
+  border: 0;
+  color: white;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${darkbrown};
+  }
+`;
+const ReviewInputTextBox = styled.div`
+  padding: 10px;
+  width: calc(100% - 70px);
+  display: flex;
+  align-items: center;
+`;
+
+const CloseBox = styled.div`
+  position: fixed;
+  z-index: 999;
+  top: 48%;
+  left: 357px;
+  bottom: 0;
+  right: 0;
+  opacity: 0.8;
+  .close {
+    cursor: pointer;
+  }
+`;
+
+const ReviewWrite = styled.div`
+  width: 100%;
+  height: 100px;
+  display: flex;
+  background-color: white;
+  border-top: 1px solid ${bordergrey};
+`;
+
+const EmptyMessage = styled.div`
+  margin-top: 35px;
+  text-align: center;
+  font-size: 14px;
+  color: ${brown};
+`;
+
+type Props = {
+  type: string;
+  placeholder: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+export default CommunityReview;
---
+++ client/src/Components/Review.tsx
@@ -0,0 +1,290 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { Icon } from '@iconify/react';
+import { Comment } from '../Pages/CommunityDetail';
+import Swal from 'sweetalert2';
+import color from '../util/color';
+import { PostReviewUPDATE, PostReviewDELETE } from '../util/PostReviewApi';
+
+type RProps = {
+  comment: Comment;
+  getData(): void;
+  editingCommentId: number;
+  setEditingCommentId(commentId: number): void;
+};
+
+const { ivory, brown, bordergrey, lightgrey, yellow, darkbrown } = color;
+const petId = Number(localStorage.getItem('petId') as string);
+
+const Review = ({ comment, getData, editingCommentId, setEditingCommentId }: RProps) => {
+  const [newText, setNewText] = useState<string>('');
+
+  const reviewEditHandler = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setNewText((e.target as HTMLInputElement).value);
+  };
+
+  const reviewUpdateHandler = (commentId: number) => {
+    if (newText === '') {
+      Swal.fire({
+        position: 'center',
+        icon: 'warning',
+        iconHtml: '⚠',
+        title: '내용을 입력해주세요. ',
+        color: brown,
+        padding: '20px 0px 40px 0px',
+      });
+      return;
+    } else {
+      Swal.fire({
+        title: '정말 수정하시겠어요?',
+        icon: 'warning',
+        showCancelButton: true,
+        color: brown,
+        confirmButtonColor: yellow,
+        cancelButtonColor: bordergrey,
+        confirmButtonText: '<b>확인</b>',
+        cancelButtonText: '<b>취소</b>',
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire({
+            title: '수정되었습니다.',
+            icon: 'success',
+            color: brown,
+            confirmButtonColor: yellow,
+            confirmButtonText: '<b>확인</b>',
+          });
+          PostReviewUPDATE(commentId, newText).then(() => getData());
+          setEditingCommentId(0);
+          setNewText('');
+        }
+      });
+    }
+  };
+
+  const reviewEditCancelHandler = () => {
+    setEditingCommentId(0);
+    setNewText('');
+  };
+
+  const reviewDeleteHandler = (commentId: number) => {
+    Swal.fire({
+      title: '정말 삭제하시겠어요?',
+      icon: 'warning',
+      showCancelButton: true,
+      color: brown,
+      confirmButtonColor: yellow,
+      cancelButtonColor: bordergrey,
+      confirmButtonText: '<b>확인</b>',
+      cancelButtonText: '<b>취소</b>',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: '삭제되었습니다.',
+          icon: 'error',
+          color: brown,
+          confirmButtonColor: yellow,
+          confirmButtonText: '<b>확인</b>',
+        });
+        PostReviewDELETE(commentId).then(() => getData());
+      }
+    });
+  };
+
+  return (
+    <Container>
+      {comment.commentId !== editingCommentId ? (
+        <>
+          <UserBox>
+            <UserImage src={comment.profileImageUrl} />
+            <UserName>{comment.petName}</UserName>
+          </UserBox>
+          <TextBox>
+            <Text>
+              {comment.content}
+              {comment.petId === petId && (
+                <EditDelButtons>
+                  <button onClick={() => setEditingCommentId(comment.commentId)}>
+                    <Icon icon='mdi:pencil' style={{ fontSize: '15px' }} />
+                  </button>
+                  <button onClick={() => reviewDeleteHandler(comment.commentId)}>
+                    <Icon
+                      icon='material-symbols:delete-outline-rounded'
+                      style={{ fontSize: '15px' }}
+                    />
+                  </button>
+                </EditDelButtons>
+              )}
+            </Text>
+            <Date>{comment.createdAt}</Date>
+          </TextBox>
+        </>
+      ) : (
+        <>
+          <UserBox>
+            <UserImage src={comment.profileImageUrl} />
+            <UserName>{comment.petName}</UserName>
+          </UserBox>
+          <InputTextBox>
+            <InputBox>
+              <Input
+                type='text'
+                placeholder={comment.content}
+                onChange={reviewEditHandler}
+                id='basereview'
+              ></Input>
+            </InputBox>
+            <CheckButton onClick={() => reviewUpdateHandler(comment.commentId)}>
+              <Icon icon='mdi:check-bold' color='#ffc57e' style={{ fontSize: '20px' }} />
+            </CheckButton>
+            <XButton onClick={reviewEditCancelHandler}>
+              <Icon icon='mdi:cancel-bold' color='#f79483' style={{ fontSize: '22px' }} />
+            </XButton>
+          </InputTextBox>
+        </>
+      )}
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  width: 100%;
+  min-height: 100px;
+  display: flex;
+  background-color: white;
+  border-top: 1px solid ${bordergrey};
+`;
+
+const UserBox = styled.div`
+  width: 70px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const UserImage = styled.img`
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  background-size: cover;
+`;
+
+const UserName = styled.div`
+  margin-top: 8px;
+  color: ${brown};
+  font-size: 14px;
+  font-weight: Bold;
+`;
+
+const TextBox = styled.div`
+  padding: 20px 15px 20px 10px;
+  width: calc(100% - 70px);
+  min-height: 110px;
+`;
+
+const Text = styled.div`
+  width: 100%;
+  height: 100%;
+  color: ${brown};
+  font-size: 14px;
+  font-weight: 500;
+
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Date = styled.div`
+  text-align: end;
+  color: ${lightgrey};
+  font-size: 11px;
+  margin-right: 7px;
+`;
+
+const InputBox = styled.div`
+  flex-grow: 1;
+  color: ${brown};
+  font-size: 14px;
+  font-weight: 500;
+`;
+
+const Input = styled.input<Props>`
+  padding: 10px;
+  width: 100%;
+  height: 50px;
+  font-size: 14px;
+  color: ${brown};
+  border: 1px solid ${bordergrey};
+  border-radius: 15px;
+  resize: none;
+
+  &:focus {
+    outline: 1px solid ${bordergrey};
+  }
+  &::placeholder {
+    color: ${lightgrey};
+  }
+`;
+const CheckButton = styled.button`
+  margin-left: 4px;
+  margin-right: 4px;
+  padding: 7px 10px;
+  font-weight: bold;
+  background: ${brown};
+  border-radius: 12px;
+  border: 0;
+  color: white;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${darkbrown};
+  }
+`;
+const InputTextBox = styled.div`
+  padding: 20px 15px 20px 10px;
+  width: calc(100% - 70px);
+  display: flex;
+  align-items: center;
+  min-height: 110px;
+`;
+
+const EditDelButtons = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  button {
+    padding: 5px;
+    border: transparent;
+    border-radius: 5px;
+    color: ${lightgrey};
+    background: none;
+    cursor: pointer;
+    line-height: 0px;
+
+    &:hover {
+      color: ${yellow};
+      background-color: ${ivory};
+    }
+  }
+`;
+
+const XButton = styled.button`
+  padding: 7px 10px;
+  font-weight: bold;
+  background: ${ivory};
+  border-radius: 12px;
+  border: 1px solid ${bordergrey};
+  color: white;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${darkbrown};
+  }
+`;
+
+type Props = {
+  type: string;
+  placeholder: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+export default Review;
