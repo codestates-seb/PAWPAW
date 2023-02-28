40d97c77bc3c1ee2f85339ba30c1c0930aaab9cb
[subimm] <[subimm630@gmail.com]>
2023-02-28 21:47:09 +0900

    [BE] feat: 회원 상태 수정

--- server/src/main/java/animalsquad/server/domain/pet/entity/PetStatus.java
+++ server/src/main/java/animalsquad/server/domain/pet/entity/PetStatus.java
@@ -7,8 +7,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum PetStatus {
     PET_ACTIVE("활동중"),
-    PET_SLEEP("휴면 상태"),
-    PET_QUIT("탈퇴 상태");
+    PET_SLEEP("탈퇴 상태");
 
     @Getter
     private final String description;
