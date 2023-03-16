d39eabc808be839ded1b3dd4f887fdcadbf857f7
LEEKH109 <dlrjsgml109@gmail.com>
2023-03-16 13:41:28 +0900

    [FE] fix : 지역명 url 코드 로직 수정

--- client/src/Pages/Community.tsx
+++ client/src/Pages/Community.tsx
@@ -34,11 +34,7 @@ const Community = () => {
   }, [page, sorting, areaSorting]);
 
   async function getData() {
-    let test = '';
-    for (const el of areaSorting) {
-      test = test + '&code=' + addressToCode(el);
-      console;
-    }
+    const area = areaSorting.reduce((total, now) => total + '&code=' + addressToCode(now), '');
     if (areaSorting.length === 0) {
       await axios
         .get(`${url}/posts?page=${page}&sort=${sorting}`, { headers })
@@ -50,7 +46,7 @@ const Community = () => {
         });
     } else {
       await axios
-        .get(`${url}/posts?page=${page}&sort=${sorting}${test}`, { headers })
+        .get(`${url}/posts?page=${page}&sort=${sorting}${area}`, { headers })
         .then((res) => {
           setPostData(res.data);
         })
